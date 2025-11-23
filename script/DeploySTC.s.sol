4f557df46cbc5569bf0e771d4c622b21b7abcf05
Dev750wala <sadisatsowaladev1@gmail.com>
2025-11-23 07:45:36 +0000

    feat: enhance deployment scripts with network configuration and mock price feeds

--- script/DeploySTC.s.sol
+++ script/DeploySTC.s.sol
@@ -4,13 +4,20 @@ pragma solidity ^0.8.28;
 import {Script} from "forge-std/Script.sol";
 import {StableCoinEngine} from "../src/StableCoinEngine.sol";
 import {StableCoin} from "../src/StableCoin.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploySTC is Script {
     function run() external returns (StableCoin, StableCoinEngine) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.activeNetworkConfig();
         vm.startBroadcast();
         StableCoin stableCoin = new StableCoin();
 
-        StableCoinEngine stableCoinEngine = new StableCoinEngine([], [], address(stableCoin));
+        StableCoinEngine stableCoinEngine = new StableCoinEngine(
+            [networkConfig.weth, networkConfig.wbtc],
+            [networkConfig.wethUsdPriceFeed, networkConfig.wbtcUsdPriceFeed],
+            address(stableCoin)
+        );
         vm.stopBroadcast();
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,29 +3,41 @@ pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
         address wethUsdPriceFeed;
-        address wbtcUsdPriceFeed; 
+        address wbtcUsdPriceFeed;
         address weth;
         address wbtc;
         uint256 deployerKey;
     }
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 10000e8;
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     NetworkConfig public activeNetworkConfig;
 
-    constructor() {}
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilConfig();
+        }
+    }
 
     function getSepoliaConfig() public view returns (NetworkConfig memory) {
-        return
-            NetworkConfig({
-                wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
-                wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
-                weth: 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9,
-                wbtc: 0x92f3B59a79bFf5dc60c0d59eA13a44D082B2bdFC,
-                deployerKey: vm.envUint("PRIVATE_KEY")
-            });
+        return NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9,
+            wbtc: 0x92f3B59a79bFf5dc60c0d59eA13a44D082B2bdFC,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
     }
 
     function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
@@ -36,19 +48,22 @@ contract HelperConfig is Script {
         console.log("Deploying mocks...");
 
         vm.startBroadcast();
-        address wethUsdPriceFeed = address(new MockV3Aggregator(18, 2000e18));
-        address wbtcUsdPriceFeed = address(new MockV3Aggregator(8, 30000e8));
-        address weth = address(new MockWETH());
-        address wbtc = address(new MockWBTC());
+        address wethUsdPriceFeed = address(new MockV3Aggregator(DECIMALS, ETH_USD_PRICE));
+        ERC20Mock weth = new ERC20Mock();
+        weth.mint(msg.sender, 1000e18);
+
+        address wbtcUsdPriceFeed = address(new MockV3Aggregator(DECIMALS, BTC_USD_PRICE));
+        ERC20Mock wbtc = new ERC20Mock();
+        wbtc.mint(msg.sender, 1000e8);
+
         vm.stopBroadcast();
 
-        return
-            NetworkConfig({
-                wethUsdPriceFeed: wethUsdPriceFeed,
-                wbtcUsdPriceFeed: wbtcUsdPriceFeed,
-                weth: weth,
-                wbtc: wbtc,
-                deployerKey: vm.envUint("PRIVATE_KEY")
-            });
+        return NetworkConfig({
+            wethUsdPriceFeed: wethUsdPriceFeed,
+            wbtcUsdPriceFeed: wbtcUsdPriceFeed,
+            weth: address(weth),
+            wbtc: address(wbtc),
+            deployerKey: DEFAULT_ANVIL_KEY
+        });
     }
 }
